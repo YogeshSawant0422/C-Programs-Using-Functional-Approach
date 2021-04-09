be8eec27ca862d565d01073e5bae5069780978da
Yogesh Sawant <yogeshsawant0422@gmail.com>
2021-04-09 23:43:21 +0530

    String Programs

--- String/13 Find The Count Of Vowels In Given String.c
+++ String/13 Find The Count Of Vowels In Given String.c
@@ -5,7 +5,23 @@
 int main()
 {
     char cSrc[20] = {'\0'};
-    int i = 0;
+    int i = 0  , Cnt = 0;
 
-    if(cSrc == 'A' || cSrc == 'E'|| )
+    printf("\n Enter A String = ");
+    gets(cSrc);
+     while(cSrc[i] != '\0')
+     {
+        if(cSrc[i] == 'A' || cSrc[i] == 'E'|| cSrc[i] == 'I' || cSrc[i] == 'O' || cSrc[i] == 'U' || cSrc[i] == 'a' || cSrc[i] =='e' || cSrc[i] =='i' ||cSrc[i] == 'o'|| cSrc[i] =='u')
+        {
+            Cnt++;
+        }
+        i++;
+     }
+
+    printf("\n  The Count Of  Vowels In Given String Is = %d  ",Cnt );
+
+    printf("\n\n Thanks !!!!");
+
+    getch();
+    return 0;
 }
