787b888e8e60ff875221922530393be55dae988e
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-06-03 23:56:06 +0530

    Assignment No 10

--- Case Study Programs/01 Mobile Number Validation Without Using Function.c
+++ Case Study Programs/01 Mobile Number Validation Without Using Function.c
@@ -10,35 +10,43 @@ int main()
     printf("\n Enter A Mobile Number To Find It Is Valid OR Not = ");
     scanf("%lld",&Mob_Number);
 
-    printf("\n To Find The Digit Count Of Given Number . ==>> \n Press Any Key To Continue");
-    getch();
-
-    Temp = Mob_Number;
-
-     if(Mob_Number == 0)
-     {
-         printf("\n\nInvalid Number !!!!");return -1;
-     }
-
-    while( Temp > 0)
+    if(Mob_Number >= '0'  || Mob_Number <= '9')
     {
-        Dig = Temp % 10;
-        Cnt++;
-        Temp = Temp / 10;
-    }
 
-    printf("\n\n The Digit Count Of The Given Number Is = %d ",Cnt);
+            printf("\n To Find The Digit Count Of Given Number . ==>> \n Press Any Key To Continue");
+            _getch();
 
-    if( Cnt == 10)
-    {
-        printf("\n\n So Given Number Is Valid Mobile Number .\n\nI Will Call You On This Number .");
+            Temp = Mob_Number;
+
+             if(Mob_Number == 0)
+             {
+                 printf("\n\nInvalid Number !!!!");return -1;
+             }
+
+            while( Temp > 0)
+            {
+                Dig = Temp % 10;
+                Cnt++;
+                Temp = Temp / 10;
+            }
+
+            printf("\n\n The Digit Count Of The Given Number Is = %d ",Cnt);
+
+            if( Cnt == 10)
+            {
+                printf("\n\n So Given Number Is Valid Mobile Number .\n\nI Will Call You On This Number .");
+            }
+            else
+            {
+                printf("\n\n Given Number Is Not Valid For Mobile Number .\n\n So Nothing Can Be Done With This Mobile Number.");
+            }
     }
     else
     {
-        printf("\n\n Given Number Is Not Valid For Mobile Number .\n\n So Nothing Can Be Done With This Mobile Number.");
+        printf("\n Invalid Input !!! There Is No Integer Number .");
     }
 
     printf("\n\n=================== Thanks For Using This Application ==================");
-    getch();
+    _getch();
     return 0;
 }
